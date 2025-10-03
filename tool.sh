826e48bc6dcc4dd06f6ee8ec7765b9f7ab1b5126
Fritzbox <mustiexxchange69@gmail.com>
2025-10-03 03:43:32 +0200

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+clear
+echo "========================"
+echo "   Wähle eine Option:"
+echo "========================"
+echo "1) test1"
+echo "2) test2"
+echo -n "Deine Eingabe: "
+read choice
+
+if [ "$choice" == "1" ]; then
+    echo "hi"
+elif [ "$choice" == "2" ]; then
+    echo "bye"
+else
+    echo "Ungültige Eingabe!"
+fi
